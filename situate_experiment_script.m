9380f4be46246eaa49d1792aedc96689d43fc9cc
Max Quinn <quinn.max@gmail.com>
2016-07-27 16:45:04 -0700

    Implemented spawning of local scouts, added an option in the parameters, seems to be working. Will work on merging it in to master next

--- situate_experiment_script.m
+++ situate_experiment_script.m
@@ -5,9 +5,9 @@
 %% define experiment settings
 
     experiment_settings = [];
-    experiment_settings.use_gui = true;
+    experiment_settings.use_gui = false;
     
-    experiment_settings.title = 'experiment_name';
+    experiment_settings.title = 'scout_spawning_test';
     experiment_settings.situations_struct = situate_situation_definitions();
     experiment_settings.situation = 'dogwalking'; 
     
@@ -17,11 +17,11 @@
 
     % (won't matter if gui is on)
     experiment_settings.num_folds           = 1;
-    experiment_settings.testing_data_max    = 3;  % empty will use as much as possible given the number of folds.
-    experiment_settings.training_data_max   = 30; % empty will use as much as possible given the number of folds. (less than 30 migth cause problems)
+    experiment_settings.testing_data_max    = 20; % empty will use as much as possible given the number of folds.
+    experiment_settings.training_data_max   = 400; % empty will use as much as possible given the number of folds. (less than 30 migth cause problems)
     
     % (won't matter if gui is on)
-    experiment_settings.run_analysis_after_completion = false;
+    experiment_settings.run_analysis_after_completion = true;
     
 
 
@@ -48,13 +48,12 @@
     
 
 
-
 %% define your training testing splits
 
-    % split_arg = [];                   % will use a random seed and make it's own training-testing folds
-    % split_arg = uigetdir(pwd);        % specify a directory containing training_testing split files that it will load and use
-    % split_arg = 1;                    % use a specific seed value
-    split_arg = '/Users/Max/Dropbox/Projects/situate/split_files_for_existing_models/';
+    %split_arg = [];                   % will use a random seed and make it's own training-testing folds
+    %split_arg = uigetdir(pwd);        % specify a directory containing training_testing split files that it will load and use
+    split_arg = 1;                    % use a specific seed value
+    %split_arg = '/Users/Max/Dropbox/Projects/situate/split_files_for_existing_models/';
     
 
     
@@ -71,7 +70,7 @@
         
     % pipeline
         % p.num_scouts = 10; % sets how many agents the pool will be initialized with, and how many it will be filled back up to. should probalby be called min_agent_pool_size
-        p.use_direct_scout_to_workspace_pipe = true; % hides stochastic agent stuff a bit, more comparable to other methods     
+        p.use_direct_scout_to_workspace_pipe = false; % hides stochastic agent stuff a bit, more comparable to other methods     
         p.refresh_agent_pool_after_workspace_change = true; % prevents us from evaluating agents from a stale distribution
     
     % object priority
@@ -86,6 +85,9 @@
     
     % check-in and tweaking
         p.use_box_adjust = false; % based on Evan's classifiers
+        
+        p.spawn_nearby_scouts_on_provisional_checkin = false;
+        
         % p.thresholds.internal_support = .25; % scout -> reviewer threshold
         % p.thresholds.total_support_provisional = .25; (search continues)
         % p.thresholds.total_support_final       = .5;  (search ends)
@@ -120,7 +122,8 @@
     p_conditions = [];
     p_conditions_descriptions = {};
     
-    description = 'salience, normals, learned mvn';
+    description = 'salience, normals, learned mvn, spawn nearby scouts';
+    temp.spawn_nearby_scouts_on_provisional_checkin = true;
     temp = p;
     temp.location_method_before_conditioning            = 'salience_blurry';
     temp.location_method_after_conditioning             = 'mvn_conditional_and_salience';
@@ -131,12 +134,13 @@
     temp.description = description;
     if isempty( p_conditions ), p_conditions = temp; else p_conditions(end+1) = temp; end
 
-    description = 'uniform, uniform, no mvn';
+    description = 'salience, normals, learned mvn, no nearby scout spawning';
+    temp.spawn_nearby_scouts_on_provisional_checkin = false;
     temp = p;
-    temp.location_method_before_conditioning            = 'uniform';
-    temp.location_method_after_conditioning             = 'uniform';
-    temp.box_method_before_conditioning                 = 'independent_uniform_log_aa';
-    temp.box_method_after_conditioning                  = 'independent_uniform_log_aa';
+    temp.location_method_before_conditioning            = 'salience_blurry';
+    temp.location_method_after_conditioning             = 'mvn_conditional_and_salience';
+    temp.box_method_before_conditioning                 = 'independent_normals_log_aa';
+    temp.box_method_after_conditioning                  = 'conditional_mvn_log_aa';
     temp.location_sampling_method_before_conditioning   = 'sampling';
     temp.location_sampling_method_after_conditioning    = 'sampling';
     temp.description = description;
@@ -162,7 +166,7 @@
    
    %% run the experiment
    
-   situate_experiment_wrapper(experiment_settings, p_conditions, situate_data_path, split_arg);
+   situate_experiment_helper(experiment_settings, p_conditions, situate_data_path, split_arg);
    
    
    
