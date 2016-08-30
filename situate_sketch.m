e0875c81e2b92ccdfac9ea3124a59ce10b389309
MelanieMitchell <mm@pdx.edu>
2016-08-30 12:39:46 -0700

    updated situate_sketch.m to fix local agent spawning bounds

--- situate_sketch.m
+++ situate_sketch.m
@@ -685,6 +685,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) - step_h;
         c0 = new_agent.box.r0rfc0cf(3);
         cf = new_agent.box.r0rfc0cf(4);
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -699,6 +700,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) + step_h;
         c0 = new_agent.box.r0rfc0cf(3);
         cf = new_agent.box.r0rfc0cf(4);
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -713,6 +715,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2);
         c0 = new_agent.box.r0rfc0cf(3) - step_w;
         cf = new_agent.box.r0rfc0cf(4) - step_h;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -727,6 +730,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2);
         c0 = new_agent.box.r0rfc0cf(3) + step_w;
         cf = new_agent.box.r0rfc0cf(4) + step_h;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -741,6 +745,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) + step_h/2;
         c0 = new_agent.box.r0rfc0cf(3) - step_w/2;
         cf = new_agent.box.r0rfc0cf(4) + step_w/2;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -755,6 +760,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) - step_h/2;
         c0 = new_agent.box.r0rfc0cf(3) + step_w/2;
         cf = new_agent.box.r0rfc0cf(4) - step_w/2;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -769,6 +775,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) + step_h/2;
         c0 = new_agent.box.r0rfc0cf(3) + step_w/2;
         cf = new_agent.box.r0rfc0cf(4) - step_w/2;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
@@ -783,6 +790,7 @@ function agent_pool = spawn_local_scouts( agent_to_expand, agent_pool, d )
         rf = new_agent.box.r0rfc0cf(2) - step_h/2;
         c0 = new_agent.box.r0rfc0cf(3) - step_w/2;
         cf = new_agent.box.r0rfc0cf(4) + step_w/2;
+        r0 = max(1,round(r0)); rf = min(d(1).image_size(1),round(rf)); c0 = max(1,round(c0)); cf = min(d(1).image_size(2),round(cf));
         x  = c0; y  = r0; w  = cf-c0+1; h  = rf-r0+1; xc = x+w/2; yc = y+w/2;
         new_agent.box.r0rfc0cf = [r0 rf c0 cf];
         new_agent.box.xywh     = [ x  y  w  h];
