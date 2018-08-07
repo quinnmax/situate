66a92cfd48d5e7317934298699d9a699c1c377e4
Max Quinn <quinn.max@gmail.com>
2018-08-07 15:17:38 -0700

    added args for pre and post conditioning probability to uniform_normal_mix

--- +situation_models/uniform_normal_mix_fit.m
+++ +situation_models/uniform_normal_mix_fit.m
@@ -1,13 +1,28 @@
 
-function model = uniform_normal_mix_fit( situation_struct, data_in, p_uniform_post_conditioning )
-% model = uniform_normal_mix_fit( situation_struct, data_in, uniform_after_conditioning_probability );
+function model = uniform_normal_mix_fit( situation_struct, data_in, varargin )
+% model = uniform_normal_mix_fit( situation_struct, data_in, p_uniform_pre_conditioning, p_uniform_post_conditioning );
+% model = uniform_normal_mix_fit( situation_struct, data_in, p_uniform_post_conditioning );
+% model = uniform_normal_mix_fit( situation_struct, data_in );
+%
+% p_uniform_pre_conditioning default is 1
+% p_uniform_post_conditioning default is .5
 
-    if ~exist('uniform_after_conditioning_probability','var') || isempty(p_uniform_post_conditioning) 
-        p_uniform_post_conditioning = .5;
+    switch length(varargin)
+        case 0
+            p_uniform_pre_conditioning  = 1;
+            p_uniform_post_conditioning = .5;
+        case 1
+            p_uniform_pre_conditioning  = 1;
+            p_uniform_post_conditioning = varargin{1};
+        case 2
+            p_uniform_pre_conditioning  = varargin{1};
+            p_uniform_post_conditioning = varargin{2};
+        otherwise
+            warning('too many input args');
     end
-
+        
     model = situation_models.normal_fit( situation_struct, data_in );
-    model.p_uniform_pre_conditioning = 1;
+    model.p_uniform_pre_conditioning  = p_uniform_pre_conditioning;
     model.p_uniform_post_conditioning = p_uniform_post_conditioning;
     model.model_description = 'uniform_normal_mix';
     
